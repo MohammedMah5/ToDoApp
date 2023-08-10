42a5e226cd349a7aa671a9d49f3c13a62161f465
Mohammed <mohmedahmeduae1@gmail.com>
2023-08-10 14:35:09 +0400

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Mohmed Ahmed on 10/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
