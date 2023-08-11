4d45e7b8f00dc558e8ce7da4eb6c03c0ba0d5cb0
Mohammed <mohmedahmeduae1@gmail.com>
2023-08-11 19:51:54 +0400

    Finished the basic checking and unchecking functionality

--- ToDoApp/ContentView.swift
+++ ToDoApp/ContentView.swift
@@ -17,8 +17,15 @@ struct ContentView: View {
     
     var body: some View {
         NavigationStack {
-            List(todos)  { todo in
-                Text(todo.title)
+            List($todos)  { $todo in
+                HStack {
+                    Image(systemName: todo.isCompleted ? "checkmark.circle.fill" : "circle")
+                        .onTapGesture {
+                            todo.isCompleted.toggle()
+                        }
+                    Text(todo.title)
+                        .strikethrough(todo.isCompleted)
+                }
             }
             .navigationTitle("Todos")
         }
